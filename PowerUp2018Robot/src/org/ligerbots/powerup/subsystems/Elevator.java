62e70776afc3fa8af3907c74c197adccaeaea22c
Ligerbots <Ligerbots@LigerBlue.newton.edu>
2018-01-13 10:25:49 -0500

    More edits to elevator

--- PowerUp2018Robot/src/org/ligerbots/powerup/subsystems/Elevator.java
+++ PowerUp2018Robot/src/org/ligerbots/powerup/subsystems/Elevator.java
@@ -20,7 +20,10 @@ public class Elevator extends Subsystem {
     public enum ElevatorPosition {
       SCALE, 
       SWITCH, 
-      EXCHANGE
+      EXCHANGE,
+      UP,
+      DOWN,
+      RESTING
     }
     
     TalonSRX elevatorMaster;
@@ -33,7 +36,8 @@ public class Elevator extends Subsystem {
     double I = 0.05;
     double D = 0.05;
     double requestedRPM=0.0;//When the elevator is not moving, the 775s should stay in place (maintain 0 RPM)
-    
+    double pidOutput;
+    double defaultRPM = 10.0;//How fast it should go when it is moved up or down
     public Elevator() {
       
       elevatorMaster = new TalonSRX(RobotMap.CT_ELEVATOR_1);
@@ -43,7 +47,26 @@ public class Elevator extends Subsystem {
       elevatorMaster.setNeutralMode(NeutralMode.Brake);
       elevatorSlave.setNeutralMode(NeutralMode.Brake);
       
-    //  elevatorController = new PIDController(P, I, D);
+    //  elevatorController = new PIDController(P, I, D, encoder, output -> pidOutput = output);
+    }
+    public void setRPM(double requestedRPM) {
+      this.requestedRPM = requestedRPM;
+    }
+    public void goUp() {
+      setRPM(defaultRPM);
+      
+    }
+    public void goDown() {
+      setRPM(-defaultRPM);
+    }
+    public void holdPosition() {
+      setRPM(0.0);
+    }
+    public void setElevatorPosition(ElevatorPosition position) {
+      switch (position) {
+        case UP:
+          setRPM(defaultRPM);
+      }
     }
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
