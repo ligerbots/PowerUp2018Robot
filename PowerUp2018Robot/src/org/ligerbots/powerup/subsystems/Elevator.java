2e3d0a924a9a73056541ffeb6dfb19862bff6f19
Matthew Leonard <matthewdleonard01@gmail.com>
2018-01-13 09:57:07 -0500

    Begin elevator subsystem
    Added some basic code to ellevator subsystem

--- PowerUp2018Robot/src/org/ligerbots/powerup/subsystems/Elevator.java
+++ PowerUp2018Robot/src/org/ligerbots/powerup/subsystems/Elevator.java
@@ -1,6 +1,8 @@
 package org.ligerbots.powerup.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.CANTalon;
+import com.ctre.phoenix.*;
 
 /**
  *
@@ -9,7 +11,29 @@ public class Elevator extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+    TalonSRX elevatorMaster;
+    TalonSRX elevatorSlave;
+    TalonID elevatorMasterTalon;
+    TalonID elevatorSlaveTalon;
+    SpeedControllerGroup speedController;
+    PIDController ElevatorController;
+    const int P 0.05;
+    const int I = 0.05;
+    const int D = 0.05;
+    double requestedRPM=0.0;//When the elevator is not moving, the 775s should stay in place (maintain 0 RPM)
+    public Elevator() {
+      elevatorMaster = TalonSRX(RobotMap.CT_ELEVATOR_1);
+      elevatorSlave = TalonSRX(RobotMap.CT_ELEVATOR_2);
+      
+      speedController = new SpeedControllerGroup((SpeedController)elevatorMaster, (SpeedController)elevatorSlave);
+      elevatorMaster.setNeutralMode(NeutralMode.Brake);
+      elevatorSlave.setNeutralMode(NeutralMode.Brake);
+      
+      elevatorMasterTalon = TalonID(RobotMap.CT_ELEVATOR_1, elevatorMaster);
+      elevatorSlaveTalon = TalonID(RobotMap.CT_ELEVATOR_2, elevatorSlave);
+      
+      elevatorController = new PIDController(P, I, D, )
+    }
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
