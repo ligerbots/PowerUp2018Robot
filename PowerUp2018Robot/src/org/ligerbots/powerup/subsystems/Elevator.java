8cd59da3d4a5e3fbde663e8dbcb5ac1a75768d28
Mark Nazzaro <marknazzaro2@gmail.com>
2018-01-13 10:06:19 -0500

    Fixed some errors and things

--- PowerUp2018Robot/src/org/ligerbots/powerup/subsystems/Elevator.java
+++ PowerUp2018Robot/src/org/ligerbots/powerup/subsystems/Elevator.java
@@ -1,8 +1,13 @@
 package org.ligerbots.powerup.subsystems;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import com.ctre.CANTalon;
-import com.ctre.phoenix.*;
+import org.ligerbots.powerup.RobotMap;
+import org.ligerbots.powerup.subsystems.DriveTrain.TalonID;
 
 /**
  *
@@ -11,28 +16,34 @@ public class Elevator extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+  
+    public enum ElevatorPosition {
+      SCALE, 
+      SWITCH, 
+      EXCHANGE
+    }
+    
     TalonSRX elevatorMaster;
     TalonSRX elevatorSlave;
     TalonID elevatorMasterTalon;
     TalonID elevatorSlaveTalon;
     SpeedControllerGroup speedController;
     PIDController ElevatorController;
-    const int P 0.05;
-    const int I = 0.05;
-    const int D = 0.05;
+    double P = 0.05;
+    double I = 0.05;
+    double D = 0.05;
     double requestedRPM=0.0;//When the elevator is not moving, the 775s should stay in place (maintain 0 RPM)
+    
     public Elevator() {
-      elevatorMaster = TalonSRX(RobotMap.CT_ELEVATOR_1);
-      elevatorSlave = TalonSRX(RobotMap.CT_ELEVATOR_2);
+      
+      elevatorMaster = new TalonSRX(RobotMap.CT_ELEVATOR_1);
+      elevatorSlave = new TalonSRX(RobotMap.CT_ELEVATOR_2);
       
       speedController = new SpeedControllerGroup((SpeedController)elevatorMaster, (SpeedController)elevatorSlave);
       elevatorMaster.setNeutralMode(NeutralMode.Brake);
       elevatorSlave.setNeutralMode(NeutralMode.Brake);
       
-      elevatorMasterTalon = TalonID(RobotMap.CT_ELEVATOR_1, elevatorMaster);
-      elevatorSlaveTalon = TalonID(RobotMap.CT_ELEVATOR_2, elevatorSlave);
-      
-      elevatorController = new PIDController(P, I, D, )
+    //  elevatorController = new PIDController(P, I, D);
     }
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
