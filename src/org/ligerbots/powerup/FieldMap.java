f21cf377cf93d80b0dcedb02c13b79ec1ab561e2
Charles Frankston <cbf@ligerbots.com>
2018-02-14 21:22:43 -0500

    Added starting positions and scoring positions to Field Map.

--- src/org/ligerbots/powerup/FieldMap.java
+++ src/org/ligerbots/powerup/FieldMap.java
@@ -4,27 +4,47 @@ import org.ligerbots.powerup.FieldPosition;
 
 /*
  * All positions are in inches
+ * 
+ * To understand this map, refer to the diagram at
+ * http://team.ligerbots.com/home/2018-game-info 
  */
 
 public class FieldMap {
 	
 	public static final double scaleScoringheight = (8.0 * 12.0);	// 8 feet high (in inches)
 	
-	public FieldPosition[] startPositions = new FieldPosition[6];	// position 0 is not used! (because the diagrom is 1-based)
+	public FieldPosition[] startPositions = new FieldPosition[5];	// position 0 is not used! (because the diagrom is 1-based)
 	
 	// The positions for our scoring positions on all platforms is symmetric around the X axis
 	// in other words, if one platform is at Y, then the other is at -Y
 	
-	public FieldPosition switchScoringSpotA;	// The "normal" switch scoring positions
-	public FieldPosition switchScoringSpotB;	// The "inner" positions, if we need to avoid our partners
-	public FieldPosition switchScoringSpotE;	// The outside edges of the switch -- to avoid our partners entirely
+	public FieldPosition[] switchScoringSpot = new FieldPosition[3];	
+	public FieldPosition[] scaleScoringSpot = new FieldPosition[2];	
+
+	
 	
 	public FieldPosition scaleScoringSpotAlpha;	// The normal "inner" scoring position
 	public FieldPosition scaleScoringSpotBeta;	// Acessing the platform from the outside
 	
 	FieldMap () {
+		startPositions[1] = new FieldPosition(0.0, 81);
+		startPositions[2] = new FieldPosition(0.0, 108);
+		startPositions[3] = new FieldPosition(0.0, -8.0);
+		startPositions[4] = new FieldPosition(0.0, -54.0);
+		startPositions[4] = new FieldPosition(0.0, -96.0);
 		
-		startPositions[1] = new FieldPosition(0.0, 76.7);
+		switchScoringSpot[0] = new FieldPosition(166.2, 119.4);
+		switchScoringSpot[1] = new FieldPosition(97.2, 54.8);
+		switchScoringSpot[2] = new FieldPosition(252.0, 54.8);
+		
+		scaleScoringSpot[0] = new FieldPosition(323.2, 134.5);
+		scaleScoringSpot[1] = new FieldPosition(254.8, 90.6);
+		
+				
+		// TODO -- create waypoints corresponding to each scoring position
+	    //         that will ensure the robot doesn't crash into things
+	    //         or enter forbidden areas
+		
 	};
 	  
 
